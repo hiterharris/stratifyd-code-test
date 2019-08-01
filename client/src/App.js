647ce3fd3265d2888e7aea303390c378618ac9d7
Hiter Harris <hiter.harris@levvel.io>
2019-08-01 12:01:57 -0400

    App header, README image

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './images/logo.svg';
+import stratifyd from './images/stratifyd-logo.png';
 import Data from './components/Data';
 import Table from './components/Table';
 import './App.css';
@@ -9,8 +10,8 @@ export default class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Express App</h2>
+          <img src={stratifyd} className="stratifyd-logo" alt="stratifyd-logo" />
+          <h2 className="App-title">React Express App</h2>
         </div>
         <Data />
         <Table />
