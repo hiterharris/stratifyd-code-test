75455f42d0e8ae2429654cb419b8bac831b66aef
Hiter Harris <hiter.harris@levvel.io>
2019-08-02 08:39:30 -0400

    removed unordered list, created table

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Table.css';
 
-export default class Table extends Component {
+export default class Test extends Component {
     constructor() {
         super();
         this.state = {
@@ -18,24 +18,22 @@ export default class Table extends Component {
     render() {
         return (
             <div>
-                <ul className="table">
-                    <ul className="table-column">
-                        <li className="table-header">Id</li>
-                        <li>{this.state.data.map(data => <div className="table-data">{data.id}</div>)}</li>
-                    </ul>
-                    <ul className="table-column">
-                        <li className="table-header">Content</li>
-                        <li>{this.state.data.map(data => <div className="table-data-content">{data.Content}</div>)}</li>
-                    </ul>
-                    <ul className="table-column">
-                        <li className="table-header">Time</li>
-                        <li>{this.state.data.map(data => <div className="table-data">{data.Time}</div>)}</li>
-                    </ul>
-                    <ul className="table-column">
-                        <li className="table-header">Category</li>
-                        <li>{this.state.data.map(data => <div className="table-data">{data.Category}</div>)}</li>
-                    </ul>
-                </ul>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Content</th>
+                        <th>Time</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{this.state.data.map(data => <div>{data.id}</div>)}</td>
+                        <td>{this.state.data.map(data => <div>{data.Content}</div>)}</td>
+                        <td>{this.state.data.map(data => <div>{data.Time}</div>)}</td>
+                        <td>{this.state.data.map(data => <div>{data.Category}</div>)}</td>
+                    </tr>
+                </tbody>
             </div>
         );
     }
